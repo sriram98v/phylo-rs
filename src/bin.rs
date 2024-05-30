86f3a00ec5bd376c00359c8a92f847ee3260640e
sriram98v <sriram.98v@gmail.com>
2024-05-30 13:17:17 -0500

    added PD to CLI

--- src/bin.rs
+++ src/bin.rs
@@ -9,11 +9,14 @@ use itertools::Itertools;
 use phylo::iter::node_iter::{Ancestors, EulerWalk};
 use phylo::tree::simple_rtree::{RootedTree, RootedMetaTree};
 use phylo::tree::distances::PathFunction;
+use tree::stats::PhylogeneticDiversity;
 use std::time::{Duration, Instant};
+use std::fs::File;
+use std::io::Read;
 use rand::prelude::IteratorRandom;
 
 fn main(){
-    let matches = Command::new("Generalized suffix tree")
+    let matches = Command::new("Phylogenetics Rust")
         .version("1.0")
         .author("Sriram Vijendran <vijendran.sriram@gmail.com>")
         .subcommand(Command::new("cophen-dist-repr")
@@ -31,6 +34,20 @@ fn main(){
             )
             .arg(arg!(-t --threads <THREADS> "number of threads")
                 .value_parser(clap::value_parser!(usize))
+            )
+        )
+        .subcommand(Command::new("PD")
+            .about("Compute Phylogenetic Diversity")
+            .subcommand(Command::new("min")
+                .about("Compute minPD")
+                .arg(arg!(-f --file <TREE_FILE> "Input Tree File")
+                    .required(true)
+                    .value_parser(clap::value_parser!(String))
+                )
+                .arg(arg!(-n --num_taxa <NUM_TAXA> "Input Tree File")
+                    .required(true)
+                    .value_parser(clap::value_parser!(usize))
+                )
             )
         )
         .about("CLI tool for quick tree operations")
@@ -80,6 +97,23 @@ fn main(){
                     }).sum::<Duration>()/(*num_trees as u32);
                 
                 println!("Mean time: {:?}", mean_dist);            
+            },
+            Some(("PD", sub_m)) => {
+                match sub_m.subcommand(){
+                    Some(("min", min_pd)) => {
+                        let mut tree_file = File::open(min_pd.get_one::<String>("file").expect("required")).unwrap();
+                        let num_taxa = min_pd.get_one::<usize>("num_taxa").expect("required");
+                        let mut trees = String::new();
+
+                        tree_file.read_to_string(&mut trees).unwrap();
+                        let tree_string = trees.split("\n").collect_vec()[0];
+                        let mut tree = SimpleRootedTree::from_newick(tree_string.as_bytes());
+                        tree.precompute_minPDs();
+                        println!("minPD: {}\nnormalized minPD: {}", tree.get_minPD(num_taxa.clone()), tree.get_norm_minPD(num_taxa.clone()));
+                        // dbg!("{}", tree);
+                    }
+                    _ => println!("No valid PD metric chosen! Refer help page (-h flag)")
+                }
             },
             _ => {
                 println!("No option selected! Refer help page (-h flag)");
