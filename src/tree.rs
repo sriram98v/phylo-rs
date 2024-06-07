1d6d53753581558ded749318469d5838ee0a90c9
sriram98v <sriram.98v@gmail.com>
2024-06-07 18:50:23 -0500

    added get min_taxa set

--- src/tree.rs
+++ src/tree.rs
@@ -31,9 +31,8 @@ pub struct SimpleRootedTree{
     precomputed_fai: Option<Vec<Option<usize>>>,    
     precomputed_da: Option<Vec<usize>>,
     precomputed_rmq: Option<BinaryRmq>,
-    precomputed_min: Vec<Vec<f32>>,
-    precomputed_norm_min: Vec<Vec<f32>>,
-    precomputed_edges_count: Vec<Vec<u32>>,
+    precomputed_min: Vec<Vec<(f32, u32)>>,
+    precomputed_norm_min: Vec<Vec<(f32, u32)>>,
 }
 
 impl SimpleRootedTree{
@@ -52,7 +51,6 @@ impl SimpleRootedTree{
             precomputed_rmq: None,
             precomputed_min: Vec::new(),
             precomputed_norm_min: Vec::new(),
-            precomputed_edges_count: Vec::new(),
         }
     }
 
@@ -304,25 +302,23 @@ impl RootedWeightedTree for SimpleRootedTree {
 impl PhylogeneticDiversity for SimpleRootedTree{
     fn precompute_minPDs(&mut self)
     {
-        let (delta_bar, delta_hat, edge_count) = self.compute_norm_min();
+        let (delta_bar, delta_hat) = self.compute_norm_min();
         self.precomputed_min = delta_bar;
         self.precomputed_norm_min = delta_hat;
-        self.precomputed_edges_count = edge_count;
     }
 
-    fn compute_norm_min(&self)->(Vec<Vec<f32>>,Vec<Vec<f32>>,Vec<Vec<u32>>)
+    fn compute_norm_min(&self)->(Vec<Vec<(f32, u32)>>,Vec<Vec<(f32, u32)>>)
     {
         let num_leaves = self.get_leaves().len();
-        let mut delta_bar = vec![vec![f32::INFINITY;num_leaves+1];self.nodes.len()];
-        let mut delta_hat = vec![vec![f32::INFINITY;num_leaves+1];self.nodes.len()];
-        let mut edge_count = vec![vec![0_u32;num_leaves+1];self.nodes.len()];
+        let mut delta_bar = vec![vec![(f32::INFINITY, 0_u32);num_leaves+1];self.nodes.len()];
+        let mut delta_hat = vec![vec![(f32::INFINITY, 0_u32);num_leaves+1];self.nodes.len()];
 
         for node in self.get_nodes(){
-            delta_bar[node.get_id()][0] = 0_f32;
-            delta_hat[node.get_id()][0] = 0_f32;
+            delta_bar[node.get_id()][0] = (0_f32, 0_u32);
+            delta_hat[node.get_id()][0] = (0_f32, 0_u32);
             if node.is_leaf(){
-                delta_bar[node.get_id()][1] = 0_f32;
-                delta_hat[node.get_id()][1] = 0_f32;
+                delta_bar[node.get_id()][1] = (0_f32, 0_u32);
+                delta_hat[node.get_id()][1] = (0_f32, 0_u32);
             }
         }
 
@@ -331,56 +327,125 @@ impl PhylogeneticDiversity for SimpleRootedTree{
                 for i in 1..std::cmp::min(num_leaves, self.get_cluster_size(node.get_id()))+1{
                     let mut min_bar = f32::INFINITY;
                     let mut min_hat = f32::INFINITY;
-                    let mut min_e = 0;
+                    let mut min_e_bar = 0_u32;
+                    let mut min_e_hat = 0_u32;
                     let node_children = node.get_children().collect_vec();
                     let x = node_children[0];
                     let y = node_children[1];
                     for r in 0..i+1{
                         let l = i-r;
-                        if delta_bar[y][r]==f32::INFINITY || delta_bar[x][l]==f32::INFINITY{
+                        if delta_bar[y][r].0==f32::INFINITY || delta_bar[x][l].0==f32::INFINITY{
                             continue;
                         }
-                        let val_bar = delta_bar[y][r] + (self.get_node(y).unwrap().get_weight().unwrap()*(std::cmp::min(r,1) as f32)) 
-                            + delta_bar[x][l] + (self.get_node(x).unwrap().get_weight().unwrap()*(std::cmp::min(l,1) as f32));
-                        let mut e: u32 = 0;
+                        let val_bar = delta_bar[y][r].0 + (self.get_node(y).unwrap().get_weight().unwrap()*(std::cmp::min(r,1) as f32)) 
+                            + delta_bar[x][l].0 + (self.get_node(x).unwrap().get_weight().unwrap()*(std::cmp::min(l,1) as f32));
+                        let mut e = 0_u32;
                         if l==0{
-                            e += edge_count[y][r] +1;
+                            e += delta_bar[y][r].1 + 1;
                         }
                         else if r==0{
-                            e += edge_count[x][l] +1;
+                            e += delta_bar[x][l].1 +1;
                         }
                         else {
-                            e += edge_count[x][l] + edge_count[y][r] + 2;
+                            e += delta_bar[x][l].1 + delta_bar[y][r].1 + 2;
                         }
                         let val_hat = val_bar/(e as f32);
                         if val_bar<min_bar{
                             min_bar = val_bar;
+                            min_e_bar = e;
                         }
                         if val_hat<min_hat{
                             min_hat = val_hat;
-                            min_e = e;
+                            min_e_hat = e;
                         }
                     }
-                    delta_bar[node.get_id()][i] = min_bar;
-                    delta_hat[node.get_id()][i] = min_hat;
-                    edge_count[node.get_id()][i] = min_e;
+                    delta_bar[node.get_id()][i] = (min_bar, min_e_bar);
+                    delta_hat[node.get_id()][i] = (min_hat, min_e_hat);
                 }
             }
         }
 
-        return (delta_bar, delta_hat, edge_count);
+        return (delta_bar, delta_hat);
     }
 
     fn get_minPD(&self, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight
     {
         
-        self.precomputed_min[self.get_root_id()][std::cmp::min(num_taxa, self.num_taxa())]
+        self.precomputed_min[self.get_root_id()][std::cmp::min(num_taxa, self.num_taxa())].0
     }
+
+    fn get_minPD_node(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight 
+    {    
+        self.precomputed_min[node_id][std::cmp::min(num_taxa, self.num_taxa())].0
+    }
+
 
     fn get_norm_minPD(&self, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight
     {
-        self.precomputed_norm_min[self.get_root_id()][std::cmp::min(num_taxa, self.num_taxa())]
+        self.precomputed_norm_min[self.get_root_id()][std::cmp::min(num_taxa, self.num_taxa())].0
     }
+
+    fn backtrack_min(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize, taxa_set: &mut Vec<<<Self as RootedTree>::Node as RootedTreeNode>::NodeID>)
+    {
+        if self.is_node_leaf(node_id){
+            taxa_set.push(node_id);
+        }
+        else{
+            let node_children = self.get_node_children_ids(node_id).collect_vec();
+            let left_child_id = node_children[0];
+            let right_child_id = node_children[1];
+            let left_edge_weight = self.get_edge_weight(node_id, left_child_id).unwrap();
+            let right_edge_weight = self.get_edge_weight(node_id, right_child_id).unwrap();
+            if self.get_minPD_node(node_id, num_taxa)==self.get_minPD_node(left_child_id, num_taxa)+left_edge_weight
+            {
+                if self.is_node_leaf(left_child_id)
+                {
+                    taxa_set.push(left_child_id);
+                }
+                else
+                {
+                    self.backtrack_min(left_child_id, num_taxa, taxa_set)
+                }
+            }
+            else if self.get_minPD_node(node_id, num_taxa)==self.get_minPD_node(right_child_id, num_taxa)+right_edge_weight
+            {
+                if self.is_node_leaf(right_child_id)
+                {
+                    taxa_set.push(right_child_id);
+                }
+                else
+                {
+                    self.backtrack_min(right_child_id, num_taxa, taxa_set)
+                }
+    
+            }
+            else{
+                // the following line needs to be checked!
+                let ((left_pd, left_edges),(right_pd, right_edges)) = (1..num_taxa).map(|l| (l, num_taxa-l))
+                    .map(|(l,r)| (self.precomputed_min[left_child_id][std::cmp::min(l, self.num_taxa())], self.precomputed_min[right_child_id][std::cmp::min(r, self.num_taxa())]))
+                    .filter(|(l,r)| l.0!=f32::INFINITY && r.0!=f32::INFINITY )
+                    // .inspect(|x| println!("{:?}", &x))
+                    .min_by(|x,y| (x.0.0+x.1.0).total_cmp(&(y.0.0+y.1.0))).unwrap();
+                let num_left_taxa = (left_edges/2) as usize + 1;
+                let num_right_taxa = (right_edges/2) as usize + 1;
+                if num_left_taxa>0
+                {
+                    self.backtrack_min(left_child_id, num_left_taxa, taxa_set);
+                }
+                if num_right_taxa>0
+                {
+                    self.backtrack_min(right_child_id, num_right_taxa, taxa_set);
+                }
+            }    
+        }
+    }
+
+    // fn get_minPD_node_num_taxa(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize)->usize
+    // {
+    //     let num_edges = self.precomputed_min[node_id][std::cmp::min(num_taxa, self.num_taxa())].1;
+    //     (num_edges as f32/2_f32) as usize
+    // }
+
 
 }
 
--- src/tree/stats.rs
+++ src/tree/stats.rs
@@ -1,4 +1,4 @@
-use crate::node::simple_rnode::RootedWeightedNode;
+use crate::node::simple_rnode::{RootedWeightedNode, RootedTreeNode};
 use crate::tree::RootedTree;
 
 pub trait PhylogeneticDiversity: RootedTree
@@ -7,8 +7,26 @@ where
 {
     fn precompute_minPDs(&mut self);
 
-    fn compute_norm_min(&self)->(Vec<Vec<f32>>,Vec<Vec<f32>>,Vec<Vec<u32>>);
+    fn compute_norm_min(&self)->(Vec<Vec<(f32, u32)>>,Vec<Vec<(f32, u32)>>);
 
     fn get_minPD(&self, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight;
     fn get_norm_minPD(&self, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight;
+
+    fn backtrack_min(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize, taxaset: &mut Vec<<<Self as RootedTree>::Node as RootedTreeNode>::NodeID>);
+
+    fn get_minPD_taxa_set_node(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize)->impl Iterator<Item = <<Self as RootedTree>::Node as RootedTreeNode>::NodeID>
+    {
+        let mut taxa_set: Vec<<<Self as RootedTree>::Node as RootedTreeNode>::NodeID> = vec![];
+        self.backtrack_min(node_id, num_taxa, &mut taxa_set);
+        return taxa_set.into_iter()
+    }
+
+    fn get_minPD_taxa_set(&self, num_taxa: usize)->impl Iterator<Item = <<Self as RootedTree>::Node as RootedTreeNode>::NodeID>
+    {
+        let mut taxa_set: Vec<<<Self as RootedTree>::Node as RootedTreeNode>::NodeID> = vec![];
+        self.backtrack_min(self.get_root_id(), num_taxa, &mut taxa_set);
+        return taxa_set.into_iter()
+    }
+
+    fn get_minPD_node(&self, node_id: <<Self as RootedTree>::Node as RootedTreeNode>::NodeID, num_taxa: usize)-><<Self as RootedTree>::Node as RootedWeightedNode>::Weight;
 }
